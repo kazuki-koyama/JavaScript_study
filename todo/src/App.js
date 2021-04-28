57b32b95fe91a42ad6677644f03f5247ba94719d
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-04-29 06:32:05 +0900

    [practice] todoリスト追加・表示

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,22 +3,23 @@ import React, { useState } from 'react';
 import './App.css';
 
 const AddTodo = props => {
+
   return (
     <>
       <input type="text" value={props.value} onChange={props.onChange} />
       <button onClick={props.onClick}>追加</button>
     </>
-  );
-};
+  )
+}
 
 const TodoElement = props => {
+
   return (
     <li>
-      {props.content}
-      <button onClick={props.onDelete}>削除</button>
+      {props.value}
     </li>
-  );
-};
+  )
+}
 
 const TodoApp = () => {
   const [value, setValue] = useState("");
@@ -27,38 +28,90 @@ const TodoApp = () => {
   const handleChange = e => setValue(e.target.value);
 
   const handleAdd = () => {
-    const newTodo = { id: todoList.length, content: value };
+    const newTodo = { id: todoList.length, value: value };
     setTodoList([...todoList, newTodo]);
     setValue("");
-  };
+  }
 
-  const handleDelete = id => {
-    const newTodoList = todoList.filter(todo => todo.id !== id);
-    setTodoList(newTodoList);
-  };
-  
   return (
     <div>
-      <h1>TODO App</h1>
+      <h1>TodoApp</h1>
       <div>
         <AddTodo
-          value={value}
-          onChange={handleChange}
-          onClick={handleAdd}
+         value={value}
+         onChange={handleChange}
+         onClick={handleAdd}
         />
         <ul>
           {todoList.map(todo => (
             <TodoElement
-              key={todo.id}
-              content={todo.content}
-              onDelete={() => handleDelete(todo.id)}
+             key={todo.id}
+             value={todo.value}
             />
           ))}
         </ul>
       </div>
     </div>
-  );
-};
+  )
+}
+
+// const AddTodo = props => {
+//   return (
+//     <>
+//       <input type="text" value={props.value} onChange={props.onChange} />
+//       <button onClick={props.onClick}>追加</button>
+//     </>
+//   );
+// };
+
+// const TodoElement = props => {
+//   return (
+//     <li>
+//       {props.content}
+//       <button onClick={props.onDelete}>削除</button>
+//     </li>
+//   );
+// };
+
+// const TodoApp = () => {
+//   const [value, setValue] = useState("");
+//   const [todoList, setTodoList] = useState([]);
+
+//   const handleChange = e => setValue(e.target.value);
+
+//   const handleAdd = () => {
+//     const newTodo = { id: todoList.length, content: value };
+//     setTodoList([...todoList, newTodo]);
+//     setValue("");
+//   };
+
+//   const handleDelete = id => {
+//     const newTodoList = todoList.filter(todo => todo.id !== id);
+//     setTodoList(newTodoList);
+//   };
+  
+//   return (
+//     <div>
+//       <h1>TODO App</h1>
+//       <div>
+//         <AddTodo
+//           value={value}
+//           onChange={handleChange}
+//           onClick={handleAdd}
+//         />
+//         <ul>
+//           {todoList.map(todo => (
+//             <TodoElement
+//               key={todo.id}
+//               content={todo.content}
+//               onDelete={() => handleDelete(todo.id)}
+//             />
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
 
 function App() {
   return (
