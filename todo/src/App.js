54d42e99443a4ad299e1e4c60cb85d4c52ca92aa
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-04-29 07:05:30 +0900

    [fix] 削除機能

--- todo/src/App.js
+++ todo/src/App.js
@@ -41,7 +41,7 @@ const TodoApp = () => {
 
   return (
     <div>
-      <h1>TodoApp</h1>
+      <h1>TODO App</h1>
       <div>
         <AddTodo
          value={value}
@@ -53,7 +53,7 @@ const TodoApp = () => {
             <TodoElement
              key={todo.id}
              value={todo.value}
-             onDelete={handleDelete(todo.id)}
+             onDelete={() => handleDelete(todo.id)}
             />
           ))}
         </ul>
@@ -61,64 +61,6 @@ const TodoApp = () => {
     </div>
   )
 }
-
-// const AddTodo = props => {
-//   return (
-//     <>
-//       <input type="text" value={props.value} onChange={props.onChange} />
-//       <button onClick={props.onClick}>追加</button>
-//     </>
-//   );
-// };
-
-// const TodoElement = props => {
-//   return (
-//     <li>
-//       {props.content}
-//       <button onClick={props.onDelete}>削除</button>
-//     </li>
-//   );
-// };
-
-// const TodoApp = () => {
-//   const [value, setValue] = useState("");
-//   const [todoList, setTodoList] = useState([]);
-
-//   const handleChange = e => setValue(e.target.value);
-
-//   const handleAdd = () => {
-//     const newTodo = { id: todoList.length, content: value };
-//     setTodoList([...todoList, newTodo]);
-//     setValue("");
-//   };
-
-//   const handleDelete = id => {
-//     const newTodoList = todoList.filter(todo => todo.id !== id);
-//     setTodoList(newTodoList);
-//   };
-  
-//   return (
-//     <div>
-//       <h1>TODO App</h1>
-//       <div>
-//         <AddTodo
-//           value={value}
-//           onChange={handleChange}
-//           onClick={handleAdd}
-//         />
-//         <ul>
-//           {todoList.map(todo => (
-//             <TodoElement
-//               key={todo.id}
-//               content={todo.content}
-//               onDelete={() => handleDelete(todo.id)}
-//             />
-//           ))}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
 
 function App() {
   return (
